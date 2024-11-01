f9d9b840f74d1cae8822850cda467e678bdbe09a
xrdn00 <reydan39@gmail.com>
2024-11-01 18:24:21 +0800

    java

---
+++ print_methods.java
@@ -0,0 +1,71 @@
+
+public class print_methods {
+    //methods override each other to input/output multiple values with different data types
+    //nextline prints
+    static void println(String in){
+        System.out.println(in);
+    }
+    static void println(int in){
+        System.out.println(in);
+    }
+    static void println(double in){
+        System.out.println(in);
+    }
+    static void println(float in){
+        System.out.println(in);
+    }
+    static void println(char in){
+        System.out.println(in);
+    }
+    static void println(boolean in){
+        System.out.println(in);
+    }
+    static void println(long in){
+        System.out.println(in);
+    }
+    static void println(short in){
+        System.out.println(in);
+    }
+    static void println(byte in){
+        System.out.println(in);
+    }
+    static void println(){
+        System.out.println();
+    }
+    //normal print
+    static void print(String in){
+        System.out.print(in);
+    }
+    static void print(int in){
+        System.out.print(in);
+    }
+    static void print(double in){
+        System.out.print(in);
+    }
+    static void print(float in){
+        System.out.print(in);
+    }
+    static void print(char in){
+        System.out.print(in);
+    }
+    static void print(boolean in){
+        System.out.print(in);
+    }
+    static void print(long in){
+        System.out.print(in);
+    }
+    static void print(short in){
+        System.out.print(in);
+    }
+    static void print(byte in){
+        System.out.print(in);
+    }
+    
+    public static void main(String args[]){
+        print("1"+1);
+        println("test");
+    }
+
+    
+    
+}
